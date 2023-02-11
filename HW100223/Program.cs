0372eb932f61383e1e421d353194d3e1160b39c3
Velmeri <danielmikhh2gmail.com>
2023-02-11 10:33:00 +0100

    Добавьте файлы проекта.

---
+++ HW100223/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW100223
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> A = new Matrix<int>();
+        }
+    }
+}
